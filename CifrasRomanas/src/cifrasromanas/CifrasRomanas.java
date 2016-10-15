f3598b382281c38efbd3e674a68ab4ba9b27fdcc
Jose <>
2016-10-15 04:21:19 +0200

    15/10. noche.

--- CifrasRomanas/src/cifrasromanas/CifrasRomanas.java
+++ CifrasRomanas/src/cifrasromanas/CifrasRomanas.java
@@ -15,7 +15,9 @@ public class CifrasRomanas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+        RomanosMetodos romano = new RomanosMetodos(56);
+        System.out.println("el numero es: "+romano.converRom());
     
+}
 }
---
+++ CifrasRomanas/src/cifrasromanas/RomanosMetodos.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cifrasromanas;
+
+/**
+ *
+ * @author alumne
+ */
+import java.util.*;
+public class RomanosMetodos {
+    
+    private int numero=0;
+    private String numRomano="";
+
+    public RomanosMetodos(int numero) {
+        this.numero=numero;
+    }
+    
+    public boolean valiadacion(){
+        if (this.numero<5000 && this.numero>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getNumRomano() {
+        return numRomano;
+    }
+        
+    public String converRom(){
+        int uni=0, dec=0, cen=0, um=0;
+         uni = numero % 10;
+         um=(numero-uni) /1000;
+         cen=(numero-uni-(um*1000)-(cen*100))/10;
+        unidades(uni);
+        centenas(cen);
+        millar(um);
+        
+        return numRomano;
+    }
+    
+    private void unidades(int unidad){
+        
+        String uniRom[]={"","I","II","III",   
+                        "IV","V","VI","VII",
+                         "VIII","IX"};
+        numRomano=uniRom[unidad];
+    }
+    private void dencenas(int decenas){
+        
+        String decRom[]={"","X","XX","XXX",   
+                        "XL","L","LX","LXX",
+                         "LXXX","XC"};
+        numRomano=decRom[decenas]+numRomano;
+    }
+    private void centenas(int centena){
+        
+        String cenRom[]={"","C","CC","CCC",   
+                        "CD","D","DC","DCC",
+                         "DCCC","CM"};
+        numRomano=cenRom[centena]+numRomano;
+    }
+    private void millar(int millar){
+        
+        String milRom[]={"","M","MM","MMM",   
+                        "","","","",
+                         "",""};
+        numRomano=milRom[millar]+numRomano;
+    }
+    
+    
+}
